5b6af9b52431c883d1232fcd637f6d69d7923826
pnuevas <pnuevas@gmail.com>
2020-04-09 22:50:05 +0100

    added program files - update_one

---
+++ JavaBattleShipArrayGame/src/ShipArray.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author melo
+ *
+ */
+public class ShipArray {
+	int[] locationCells = new int[7];
+	int numOfHits = 0;
+	
+	void setLocaionCells() {
+		
+		//random number generate here
+		
+		//while loop
+		//adds two to the array argument passed
+		//so
+	}
+	
+	String checkYourself() {
+		return "hit";
+	}
+	
+}
---
+++ JavaBattleShipArrayGame/src/SimpleBattleShipGame.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+
+/**
+ * @author melo
+ * this is a simple version of a battleship game
+ * using one row array
+ * the purpose of this is to build the program from simple tasks
+ * like use input and array population.
+ *
+ */
+public class SimpleBattleShipGame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//create one random number generator
+		//add two to it
+		//or implement the add two inside the setLocation method
+		//pass the rannumber to the method
+		//inside the method 
+		//the array instance variable is populated using the number passed
+		//as the array instance variable element ... 
+		//int x while(x<2){
+		//setArrayElement[rannumber]
+		//x++}
+
+	}
+
+}
